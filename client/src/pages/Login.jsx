8a4c16732a078950f398c243443799d108fc0775
Shorya <shorya2113081@akgec.ac.in>
2024-03-03 03:39:16 +0530

    login page + few enhancements

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,10 +7,14 @@ import { useDispatch } from "react-redux";
 import { setUserInfo } from "../redux/reducers/rootSlice";
 import jwt_decode from "jwt-decode";
 import fetchData from "../helper/apiCall";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import LOG from "../images/logo.svg"
 
 axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
 
 function Login() {
+  const erremail=false;
   const dispatch = useDispatch();
   const [formDetails, setFormDetails] = useState({
     email: "",
@@ -81,47 +85,125 @@ function Login() {
   };
 
   return (
-    <section className="register-section flex-center">
-      <div className="register-container flex-center">
-        <h2 className="form-heading">Sign In</h2>
-        <form
-          onSubmit={formSubmit}
-          className="register-form"
-        >
-          <input
-            type="email"
-            name="email"
-            className="form-input"
-            placeholder="Enter your email"
+    <div className="fullsc">
+      <div className="l1_mainbody">
+        <div className="l1_left_side">
+          <div className="l1_left_logo">
+          <img src={LOG} alt="Healthy"/>
+          </div>
+          <div className="l1_left_cont">
+          <span id="welcome">Welcome </span> to the Swastika
+          </div>
+        </div>
+        <div className="l1_right_side">
+          <div className="l1_container">
+            <div className="head_log">Login</div>
+            
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": {
+                  m: 2,
+                  width: "40ch",
+                  "@media (max-width: 410px)": {
+                    width: "30ch",
+                  },
+                  "@media (max-width: 338px)": {
+                    width: "28ch",
+                  },
+                  "@media (max-width: 320px)": {
+                    width: "26ch",
+                  },
+                  "@media (max-width: 300px)": {
+                    width: "23ch",
+                  },
+                },
+              }}
+              noValidate
+              autoComplete="off"
+              className="input"
+            >
+              <TextField
+                required
+                // id="standard-required"
+                label="Email"
+                variant="outlined"
+                type="name"
+                //   error={errpass}
+                //   helperText={errpass ? "Your password is weak" : ""}
             value={formDetails.email}
             onChange={inputChange}
-          />
-          <input
-            type="password"
-            name="password"
-            className="form-input"
-            placeholder="Enter your password"
-            value={formDetails.password}
+                InputProps={{
+                  style: { color: "black" },
+                  classes: {
+                    notchedOutline: erremail ? "red-border" : "black-border",
+                  },
+                }}
+                InputLabelProps={{ style: { color: "black" } }}
+              />
+                
+            </Box>
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": {
+                  m: 2,
+                  width: "40ch",
+                  "@media (max-width: 410px)": {
+                    width: "30ch",
+                  },
+                  "@media (max-width: 338px)": {
+                    width: "28ch",
+                  },
+                  "@media (max-width: 320px)": {
+                    width: "26ch",
+                  },
+                  "@media (max-width: 300px)": {
+                    width: "23ch",
+                  },
+                },
+              }}
+              noValidate
+              autoComplete="off"
+              className="input"
+            >
+              <TextField
+                required
+                // id="standard-required"
+                label="Password"
+                variant="outlined"
+                type="password"
+                //   error={errpass}
+                //   helperText={errpass ? "Your password is weak" : ""}
+                value={formDetails.password}
             onChange={inputChange}
-          />
-          <button
-            type="submit"
-            className="btn form-btn"
-          >
-            sign in
-          </button>
-        </form>
-        <p>
-          Not a user?{" "}
-          <NavLink
-            className="login-link"
-            to={"/register"}
-          >
-            Register
-          </NavLink>
-        </p>
+                InputProps={{
+                  style: { color: "black" },
+                  classes: { notchedOutline: "black-border" },
+                }}
+                InputLabelProps={{ style: { color: "black" } }}
+              />
+            </Box>
+            <div className="forgot_text">
+              {/* <Link to="/forgotpassword" id="fglink">
+                Forgot Password?
+              </Link> */}
+            </div>
+            <div className="sub_btn_log">
+              <button type="submit" onClick={formSubmit}>
+                 <>Login</>
+              </button>
+            </div>
+            <div className="bott_text">
+              <p>Don't have an account?</p>
+              <NavLink to="/register" id="bottnav">
+                Create Account
+              </NavLink>
+            </div>
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
   );
 }
 
