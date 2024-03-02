c56c95bc81058b7f6c2c9322258f69b7acf4f9a1
tushar-glitch <tusharc20001@gmail.com>
2024-03-02 19:52:26 +0530

    Db connection moved to different folder for smooth functioning using client

--- server/index.js
+++ server/index.js
@@ -2,18 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
+require("./db/conn");
 const userRouter = require("./routes/userRoutes");
 const app = express()
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-mongoose.set('strictQuery', true);
-mongoose.connect(`mongodb+srv://tushar-glitch:${process.env.MONGO_PASS}@cluster0.fjkozqb.mongodb.net/?retryWrites=true&w=majority`)
-const db = mongoose.connection;
-db.on('open', () => {
-    console.log('DB connected successfully');
-})
+
 app.use("/api/user", userRouter);
 app.get('/', (req, res) => {
     res.send("Hello World!!")
